635d29d0b69187fbb12e8e3e5f9530d69ea0393f
Torben Jost <torbenjost@proton.me>
2024-12-03 12:23:33 +0100

    add price, total price, quantity and implement functionality

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -1,15 +1,35 @@
 package org.example;
 
+import java.math.BigDecimal;
 import java.util.List;
+public record Order(String orderId, List<OrderItem> items, BigDecimal totalPrice) {
 
-public record Order(String orderId, List<Product> products) {
+    public Order(String orderId, List<OrderItem> items) {
+        this(orderId, items, calculateTotalPrice(items));
+    }
+
+    private static BigDecimal calculateTotalPrice(List<OrderItem> items) {
+        BigDecimal total = BigDecimal.ZERO;
+        for (OrderItem item : items) {
+            total = total.add(item.getItemTotalPrice());
+        }
+        return total;
+    }
 
     public Order withOrderId(String orderId) {
-        return new Order(orderId, products);
+        return new Order(orderId, items, totalPrice);
     }
 
-    public Order withProducts(List<Product> products) {
-        return new Order(orderId, products);
+    public Order withItems(List<OrderItem> items) {
+        return new Order(orderId, items, calculateTotalPrice(items));
     }
 
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId='" + orderId + '\'' +
+                ", items=" + items +
+                ", totalPrice=" + totalPrice +
+                '}';
+    }
 }
